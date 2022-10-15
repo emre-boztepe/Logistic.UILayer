40a90250e615c6c580990a2810c212d5a62041ba
emre-boztepe <emre_boztepe@windowslive.com>
2022-10-15 14:47:31 +0300

    Add project files.

---
+++ Logistic.UILayer/Controllers/CategoryController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Logistic.UILayer.Models;
+
+namespace Logistic.UILayer.Controllers
+{
+    public class CategoryController : Controller
+    {
+      DbLogisticEntities db = new DbLogisticEntities();
+        public ActionResult Index()
+        {
+           var values = db.TblCategory.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult AddCategory(TblCategory p)
+        {
+            db.TblCategory.Add(p);
+            db.SaveChanges();
+            return View();
+        }
+
+    }
+}
---
+++ Logistic.UILayer/Controllers/RegisterController.cs
@@ -0,0 +1,38 @@
+﻿using Logistic.UILayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Logistic.UILayer.Controllers
+{
+    public class RegisterController : Controller
+    {
+        DbLogisticEntities db = new DbLogisticEntities();
+
+
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Index(TblCustomer p)
+        {
+            db.TblCustomer.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Login");
+        }
+
+
+        public PartialViewResult PartialHead()
+    {
+        return PartialView();
+    }
+  }
+}
